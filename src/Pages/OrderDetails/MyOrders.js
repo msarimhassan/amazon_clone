b461f82f138b1a3e14834e32139dbb77b19bf957
sarim-dev <sarimhassan5@gmail.com>
2022-07-14 08:57:49 +0500

    Fix Order API

--- src/Pages/OrderDetails/MyOrders.js
+++ src/Pages/OrderDetails/MyOrders.js
@@ -1,23 +1,20 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import { Button } from 'reactstrap';
-
 
 import { ACNetwork, Urls, config } from '../../config';
 import Loader from '../../assets/animations';
 import DropDown from './DropDown';
 import NoData from '../../components/NoData';
 
-
 export default function MyOrders() {
-
-      const [userOrders, setUserOrders] = useState([]);
-      const [loading, setLoading] = useState(true);
-      const [page, setPages] = useState(1);
+    const [userOrders, setUserOrders] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [page, setPages] = useState(1);
     const [totalPages, setTotalPages] = useState(1);
     const getOrder = async (page) => {
         setLoading(true);
         const response = await ACNetwork.get(
-            Urls.getOrders + `?page=${page}&limit=${totalPages}`,
+            Urls.getOrders + `?page=${page}&limit=10`,
             (
                 await config()
             ).headers
@@ -29,33 +26,32 @@ export default function MyOrders() {
 
     useEffect(() => {
         getOrder(page);
-    },[])
+    }, []);
     return (
-    
-      <>
-          {loading ? (
-              <Loader />
-          ) : (
-              <>
-                  {userOrders?.length > 0 ? (
-                      userOrders.map((order) => {
-                          return <DropDown order={order} />;
-                      })
-                  ) : (
-                      <NoData />
-                  )}
-              </>
-          )}
-          {page < totalPages && (
-              <Button
-                  onClick={() => {
-                      setPages(page + 1);
-                      getOrder(page + 1);
-                  }}
-              >
-                  More
-              </Button>
-          )}
-      </>
-  );
+        <>
+            {loading ? (
+                <Loader />
+            ) : (
+                <>
+                    {userOrders?.length > 0 ? (
+                        userOrders.map((order) => {
+                            return <DropDown order={order} />;
+                        })
+                    ) : (
+                        <NoData />
+                    )}
+                </>
+            )}
+            {page < totalPages && (
+                <Button
+                    onClick={() => {
+                        setPages(page + 1);
+                        getOrder(page + 1);
+                    }}
+                >
+                    More
+                </Button>
+            )}
+        </>
+    );
 }
